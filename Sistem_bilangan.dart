5a7cc0b84bf7507cb51c3656c2eb1dcb55d5b246
dewirosariputri123-sys <dewirosariputri123@gmail.com>
2026-09-15 23:34:01 +0700

    update sistem bilangan

--- Sistem_bilangan.dart
+++ Sistem_bilangan.dart
@@ -3,7 +3,7 @@ int decimal = 10;
 int octal = int.parse('12', radix: 8);
 int binary = int.parse('1010', radix: 2);
 int hexadecimal = 0xA;
-print("Sistem Bilangan Dewi Rosari Putri");
+print("Sistem Bilangan Dewi Rosari Putri - Dev");
 print(decimal);
 print(binary);
 print(hexadecimal);
